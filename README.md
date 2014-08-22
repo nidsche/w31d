a212cbc39c683c2c94f57c24b2c5a304a46db516
Norbert Schmitz <git@norbert-schmitz.de>
2014-08-22 16:44:35 +0200

    Update 1

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ w31d
 ====
 
 TestEntry
+
+https://www.owasp.org/index.php/Fingerprint_Web_Application_Framework_%28OTG-INFO-008%29
+
+https://www.owasp.org/index.php/Testing_for_Web_Application_Fingerprint_%28OWASP-IG-004%29
+
+http://blindelephant.sourceforge.net/
+
+http://projects.webappsec.org/w/page/13246925/Fingerprinting
+
+http://sebug.net/paper/other/Web_Application_Finger_Printing.pdf
+
+http://pentestlab.wordpress.com/2012/08/01/web-application-fingerprinting/
+
+http://dcid.me/texts/fingerprinting-web-apps.html
+
+https://capec.mitre.org/data/definitions/170.html
+
+http://blog.spiderlabs.com/2012/12/class-101-automating-the-process-of-fingerprinting-web-applications-and-identifying-vulnerabilities.html
+
+
--- w31d.py
+++ w31d.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import io
+import re
 #from ConfigParser import SafeConfigParser
 import ConfigParser
 import urllib2
@@ -11,11 +12,17 @@ from optparse import OptionGroup
 options = OptionParser(usage='%prog Target_Definition', version="%prog 0.0", description='Identify applications running on a webserver')
 group1 = OptionGroup(options,"Target definitions")
 group1.add_option('--url', '-u', dest="url", default=[], action="append", help="URL to test")
+group1.add_option("--brute-dir", action="store_true", dest="brute_dir", default=False, help="Try to bruteforce directories")
+group1.add_option("--brute-dirlist", dest="brute_dirlist", default=False, help="Try to bruteforce directories")
 options.add_option_group(group1)
 opts, args = options.parse_args()
 
 ## Default Config Section
 config = ConfigParser.SafeConfigParser(defaults = {'user-agent': 'Mozilla/5.0'})
+
+alphabet="abcdefghijklmnopqrstuvwxyz0123456789"
+
+fdlist=[]
 
 debug_lvl=20
 
@@ -37,6 +44,7 @@ def evaluateHeader(header):
         print "evaluateHeader: To be implemented"
 
 
+
 def retrieveUrl(myurl,  ref='NONE'):
     req = urllib2.Request(myurl)
     # XXX here we need to use the value from config
@@ -46,16 +54,71 @@ def retrieveUrl(myurl,  ref='NONE'):
     response = urllib2.urlopen(req)
     return response
 
+def handleErrorCode(url,  code):
+    if code==403:
+        print "Found forbidden URL: "+url
+        fdlist.append(url)
+    elif code==404:
+        pass
+    else:
+        print "Unhandeled Error Code: " + str(code)
 
+def bruteDir(myurl,  depth):
+    if depth==0:
+        if debug_lvl>20:
+            print "trying "+myurl
+        try:
+            res = retrieveUrl(myurl)
+            if res.getcode() == 200:
+                print "Found " +myurl
+                fdlist.append(myurl)
+        except urllib2.HTTPError as e:
+            handleErrorCode(myurl,  e.code)
+        try:
+            res = retrieveUrl(myurl+"/")
+            if res.getcode() == 200:
+                print "Found " +myurl+"/"
+                fdlist.append(myurl+"/")
+        except urllib2.HTTPError as e:
+            handleErrorCode(myurl+"/",  e.code)
+    else:
+        for i in alphabet:
+            bruteDir(myurl+i,  depth-1)
+
+def printFilesFound():
+    print "Found the following Files/Directories"
+    for i in fdlist:
+        print i
 
 def main():
     readConfig()
     #loadPlugins()
     print opts.url
     for url in opts.url:
-        res = retrieveUrl(url)
-        print res.getcode()
-        print res.info()
+        #res = retrieveUrl(url)
+        #print res.getcode()
+        #print res.info()
+        if opts.brute_dir:
+            j=0
+            while True:
+                print "Bruting "+url+" depth "+str(j)
+                bruteDir(url, j)
+                j=j+1
+        if len(opts.brute_dirlist) > 0:
+            fi = open(opts.brute_dirlist, "r")
+            for line in fi:
+                line=line.replace("\n","")
+                line=line.replace("\r","")
+                if(len(line)>0):
+                    if line[0]=="#":
+                        pass
+                    else:
+                        #print url+line
+                        bruteDir(url+line, 0)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        print "Cancelled by user"
+        printFilesFound()
